99e00b65a869bc71d91df151db27314f9e6e24e5
Richard Tobin <richard@inf.ed.ac.uk>
2015-10-06 16:03:15 +0100

    Use real server instead of localhost in links

--- scase-wp3-nlp-server/src/main/java/nlp/Info.java
+++ scase-wp3-nlp-server/src/main/java/nlp/Info.java
@@ -2,9 +2,11 @@ package nlp;
 
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
+import javax.ws.rs.core.Context;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
+import javax.ws.rs.core.UriInfo;
 
 import org.codehaus.jettison.json.JSONException;
 import org.codehaus.jettison.json.JSONObject;
@@ -25,17 +27,18 @@ public class Info {
 	 */
 	@GET
 	@Produces(MediaType.APPLICATION_JSON)
-	public Response info() {
+	public Response info(@Context UriInfo ui) {
 		JSONObject jsonResponse = null;
 		try {
+		        String base = ui.getBaseUri().toString();
 			jsonResponse = new JSONObject();
 			jsonResponse.put("module", "NLP Server");
 			jsonResponse.put("description", "NLP Server of the EU-funded project S-CASE. See http://www.scasefp7.eu/");
 			JSONObject links = new JSONObject();
-			links.put("phrase", "http://localhost:8010/nlpserver/phrase");
-			links.put("sentence", "http://localhost:8010/nlpserver/sentence");
-			links.put("project", "http://localhost:8010/nlpserver/project");
-			links.put("question", "http://localhost:8010/nlpserver/question");
+			links.put("phrase", base + "phrase");
+			links.put("sentence", base + "sentence");
+			links.put("project", base + "project");
+			links.put("question", base + "question");
 			jsonResponse.put("_links", links);
 		} catch (JSONException e) {
 			e.printStackTrace();
